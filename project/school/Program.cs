463f60caad4d38042af624ccffba7af616379cfb
Jffmvargas <mesajjefer2@gmail.com>
2023-07-15 13:38:56 -0500

    start project of school

---
+++ project/school/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using static System.Console;
+namespace School
+
+{
+    public class School
+    {
+        private string nameSchool;
+
+
+        public School() { }
+        public String name
+        {
+            set { this.nameSchool = value; }
+            get { return nameSchool; }
+        }
+        public int yearOfCreation { get; set; }
+        public static void Main(string[] args)
+        {
+            School first = new School();
+            first.yearOfCreation = 2021;
+            first.name = "My School";
+
+            WriteLine(first.yearOfCreation);
+            WriteLine(first.name);
+        }
+
+
+
+    }
+}
+
